e9ffad03adbf6085d3fb48bed4197b84c0f3ca54
Mikhail Ershov <misha-ershov@ya.ru>
2019-09-29 13:46:35 +0300

    fix json

--- app/src/main/java/com/sdimdev/nnhackaton/data/persistence/entity/mobile/Coord.kt
+++ app/src/main/java/com/sdimdev/nnhackaton/data/persistence/entity/mobile/Coord.kt
@@ -3,6 +3,7 @@ package com.sdimdev.nnhackaton.data.persistence.entity.mobile
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "ScanInfo")
 class ScanInfo(
@@ -11,12 +12,15 @@ class ScanInfo(
         @ColumnInfo(name = "operator")
         var operator: String,
         @ColumnInfo(name = "SignalStrength")
+        @SerializedName("Level")
         var strength: String?,
         @ColumnInfo(name = "NetworkType")
         var networkType: String?,
         @ColumnInfo(name = "Latitude")
+        @SerializedName("Lat")
         var lat: Double? = null,
         @ColumnInfo(name = "Longidute")
+        @SerializedName("Lng")
         var lon: Double? = null,
         @ColumnInfo(name = "MobileId")
         val mobileId: String? = null,
